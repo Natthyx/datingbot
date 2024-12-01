6241ca3d998470b1d297595885794764016481e7
Natthyx <natannanz16@gmail.com>
2024-12-01 21:13:45 +0300

    feat:Add keep-alive ping function

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ import dotenv from "dotenv";
 import TelegramBot from "node-telegram-bot-api";
 import connectDB from "./services/database.js";
 import registrationRoutes from "./routes/botRoutes.js";
+import fetch from "node-fetch";
 
 dotenv.config();
+const app = express();
+app.use(bodyParser.json());
 
 (async () => {
   await connectDB();
@@ -15,13 +18,14 @@ dotenv.config();
   const url = process.env.WEBHOOK_URL || "https://aastucupid.onrender.com";
   await bot.setWebHook(`${url}/bot${process.env.TELEGRAM_TOKEN}`);
 
-  const app = express();
-  app.use(bodyParser.json());
-
   app.post(`/bot${process.env.TELEGRAM_TOKEN}`, (req, res) => {
     bot.processUpdate(req.body);
     res.sendStatus(200);
   });
+  // Keep the server awake
+  setInterval(() => {
+    fetch(url).then(() => console.log("Keep-alive ping sent."));
+  }, 5 * 60 * 1000); // Ping every 5 minutes
 
   registrationRoutes(bot);
 
